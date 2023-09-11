c2f5e90815c8148f5819a8707dbc2859178d5cf6
Martina Brehm <48300488+martinakbrehm@users.noreply.github.com>
2023-09-11 17:23:05 -0300

    Add files via upload
    With pagination

--- amazon_audible_bot.py
+++ amazon_audible_bot.py
@@ -8,19 +8,34 @@ driver = webdriver.Chrome()
 driver.get(web)
 driver.maximize_window()
 
-container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
-container1 = container.find_element(By.XPATH, './/ul')
-products = container1.find_elements(By.XPATH, './li')
+#pagination
+pagination = driver.find_element(By.XPATH,'//ul[contains(@class, "pagingElements")]')
+pages = pagination.find_elements(By.TAG_NAME,'li')
+last_page = int(pages[-2].text)
+next_page = driver.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
+next_page.click()
 
+current_page = 1
 book_title = []
 book_author = []
 book_length = []
 
-for product in products:
-    book_title.append(product.find_element(By.XPATH,'.//h3[contains(@class, "bc-heading")]').text)
-    book_author.append(product.find_element(By.XPATH,'.//li[contains(@class, "authorLabel")]').text)
-    book_length.append(product.find_element(By.XPATH,'.//li[contains(@class, "runtimeLabel")]').text)
+while current_page <= last_page:
 
+    container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
+    container1 = container.find_element(By.XPATH, './/ul')
+    products = container1.find_elements(By.XPATH, './li')
+
+    for product in products:
+        book_title.append(product.find_element(By.XPATH,'.//h3[contains(@class, "bc-heading")]').text)
+        book_author.append(product.find_element(By.XPATH,'.//li[contains(@class, "authorLabel")]').text)
+        book_length.append(product.find_element(By.XPATH,'.//li[contains(@class, "runtimeLabel")]').text)
+    current_page = current_page + 1
+    try:
+        next_page = driver.find_element(By.XPATH, '//span[contains(@class, "nextButton")]')
+        next_page.click()
+    except:
+        pass
 driver.quit()
 
 df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'lenght': book_length})
